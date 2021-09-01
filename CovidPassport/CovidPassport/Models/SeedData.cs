5fb2956bd9296848af01955584cfd651462a57ff
jkarlokc <jkarlokc@gmail.com>
2021-09-01 14:03:37 +0100

    SeedDataMethodStarted

--- CovidPassport/CovidPassport/Models/SeedData.cs
+++ CovidPassport/CovidPassport/Models/SeedData.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace CovidPassport.Pages.Models
 {
     public static class SeedData
     {
-
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new PassportTrackerContext(
+                serviceProvider.GetRequiredService<DbContextOptions<PassportTrackerContext>>()))
+            {
+                
+            }
+        }
     }
 }
