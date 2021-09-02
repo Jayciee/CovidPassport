dc1c0db7179c708a218e98b3b887e7b6255b9ebe
jkarlokc <jkarlokc@gmail.com>
2021-09-02 22:19:54 +0100

    Fixed Address Regex

--- CovidPassport/CovidPassport/Models/Address.cs
+++ CovidPassport/CovidPassport/Models/Address.cs
@@ -27,7 +27,7 @@ namespace CovidPassport
         public string City { get; set; }
         [Required(ErrorMessage = "Missing Postcode.")]
         [StringLength(8)]
-        [RegularExpression(@"[A-Z0-9]{2,3,4}\s[A-Z0-9]{3}",ErrorMessage = "Invalid postcode. Correct format should be in [XXXX XXX] or [XXX XXX]. Make sure there are no Special symbols or characters")]
+        [RegularExpression(@"[A-Z0-9]{3,4}\s[A-Z0-9]{3}",ErrorMessage = "Invalid postcode. Correct format should be in [XXXX XXX] or [XXX XXX]. Make sure there are no Special symbols or characters")]
         public string Postcode { get; set; }
         public string CompleteAddress 
         { 
