c28535522db06399eb0128b51e3e0d5a4de96526
jkarlokc <jkarlokc@gmail.com>
2021-09-02 15:22:00 +0100

    Redid Address Regex and Persons now displays full address when creating

--- CovidPassport/CovidPassport/Models/Address.cs
+++ CovidPassport/CovidPassport/Models/Address.cs
@@ -27,9 +27,16 @@ namespace CovidPassport
         public string City { get; set; }
         [Required(ErrorMessage = "Missing Postcode.")]
         [StringLength(8)]
-        [RegularExpression(@"[A-Z0-9]{3,4}\s[A-Z0-9]{3}",ErrorMessage = "Invalid postcode. Correct format should be in [XXXX XXX] or [XXX XXX]. Make sure there are no Special symbols or characters")]
+        [RegularExpression(@"[A-Z0-9]{2,3,4}\s[A-Z0-9]{3}",ErrorMessage = "Invalid postcode. Correct format should be in [XXXX XXX] or [XXX XXX]. Make sure there are no Special symbols or characters")]
         public string Postcode { get; set; }
-
+        public string CompleteAddress 
+        { 
+            get
+            {
+                return HouseNumber + " " + StreetName + ", " + City +", " +Postcode;
+            }
+                
+        }
         public virtual ICollection<HealthCentre> HealthCentres { get; set; }
         public virtual ICollection<Person> People { get; set; }
     }
--- CovidPassport/CovidPassport/Pages/Persons/Create.cshtml.cs
+++ CovidPassport/CovidPassport/Pages/Persons/Create.cshtml.cs
@@ -21,7 +21,7 @@ namespace CovidPassport.Pages.Persons
         public IActionResult OnGet()
         {
             
-        ViewData["AddressId"] = new SelectList(_context.Addresses, "AddressId", "City");
+        ViewData["AddressId"] = new SelectList(_context.Addresses, "AddressId", "CompleteAddress");
             return Page();
         }
 
