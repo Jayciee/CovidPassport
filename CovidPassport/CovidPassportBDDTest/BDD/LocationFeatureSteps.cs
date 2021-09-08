aafaa0b63f20c7fc9c7bf0c4d74cabc0cde636f4
Lenzed26 <denzeklfc99@gmail.com>
2021-09-08 19:09:34 +0100

    Added more tests to address, created location tests

---
+++ CovidPassport/CovidPassportBDDTest/BDD/LocationFeatureSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using CovidPassportBDDTest.libs;
+
+namespace CovidPassportBDDTest.BDD
+{
+    [Binding]
+    public class LocationFeatureSteps
+    {
+        private CovidPassport_Website<ChromeDriver> _website = new CovidPassport_Website<ChromeDriver>();
+
+        [Given(@"I am on the locations page")]
+        public void GivenIAmOnTheLocationsPage()
+        {
+            _website.LocationPage.VisitLocationPage();
+        }
+
+        [When(@"I click the create button")]
+        public void WhenIClickTheCreateButton()
+        {
+            _website.LocationPage.ClickAddCentre();
+        }
+
+        [AfterScenario]
+        public void DisposeWebDriver()
+        {
+            _website.Driver.Quit();
+            _website.Driver.Dispose();
+        }
+    }
+}
