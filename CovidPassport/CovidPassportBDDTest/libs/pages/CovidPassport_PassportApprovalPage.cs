ead8c64d729a8f4bd00d902fb9481cc1b0c74dff
keags14 <keagangonsalves_18@hotmail.com>
2021-09-08 18:53:35 +0100

    Added approval page

---
+++ CovidPassport/CovidPassportBDDTest/libs/pages/CovidPassport_PassportApprovalPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidPassportBDDTest.libs.pages
+{
+    public class CovidPassport_PassportApprovalPage
+    {
+        public CovidPassport_PassportApprovalPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public IWebDriver Driver { get; }
+
+        #region Properties
+        private string _url => AppConfigReader.ApprovalUrl;
+        #endregion
+
+        #region Methods
+
+        #endregion 
+    }
+}
